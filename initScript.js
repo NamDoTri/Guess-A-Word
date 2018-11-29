48d253fb9e5af49082eee7649b9d20baecd41d62
Nam Do Tri <t8dotr00@students.oamk.fi>
2018-11-29 19:56:25 +0200

    ADDED qwerty array

--- initScript.js
+++ initScript.js
@@ -43,4 +43,9 @@ function handleButtons(pressedButton)
     document.getElementById("secretWord").innerHTML = guessedLetters.join(" ");
     document.getElementById("numberGuesses").innerHTML = "Guesses: " + numberGuesses;
 }
+const keyboard = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A','S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z','X', 'C', 'V','B', 'N', 'M'];
+for (let i = 0; i < keyboard.length; i ++)
+{
+    document.write("<button class=\"Buttons\" onclick=\"handleButtons(this)\">" + keyboard[i] + "</button> <div class=\"divider\"></div>" );
+}
 
