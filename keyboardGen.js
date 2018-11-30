6d68f765f1e86b5ac6cb236df64096811ce1faca
Nam Do Tri <t8dotr00@students.oamk.fi>
2018-11-30 10:20:42 +0200

    only display qwerty keyboard on mobile devices

--- keyboardGen.js
+++ keyboardGen.js
@@ -1,9 +1,17 @@
-const keyboard = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A','S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z','X', 'C', 'V','B', 'N', 'M'];
-for (let i = 0; i < keyboard.length; i ++)
+const Qkeyboard = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A','S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z','X', 'C', 'V','B', 'N', 'M'];
+const alphabet =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W','X', 'Y', 'Z'];
+for (let i = 0; i < Qkeyboard.length; i ++)
 {
-    document.write("<button class=\"Buttons\" onclick=\"handleButtons(this)\">" + keyboard[i] + "</button> <div class=\"divider\"></div>");
-    if ( keyboard[i] == 'P' || keyboard[i] == 'L')
+    if (screen.width < 1400)
     {
-        document.write("<br>");
+        document.write("<button class=\"Buttons\" onclick=\"handleButtons(this)\">" + Qkeyboard[i] + "</button> <div class=\"divider\"></div>");
+        if ( Qkeyboard[i] == 'P' || Qkeyboard[i] == 'L')
+        {
+            document.write("<br>");
+        }
     }
+    else if (screen.width > 1400)
+    {
+        document.write("<button class=\"Buttons\" onclick=\"handleButtons(this)\">" + alphabet[i] + "</button> <div class=\"divider\"></div>");    
+    }   
 }
